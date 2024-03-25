2d090d75a6a9412c45d877cc83b266464ffc88c3
Rojo-Web <santifa2004@gmail.com>
2024-03-25 16:31:30 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -75,7 +75,11 @@ class ComunaController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+        return view('comuna.edit',['comuna'=> $comuna,'municipios'=> $municipios]);
     }
 
     /**
@@ -87,7 +91,18 @@ class ComunaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi','=','tb_municipio.muni_codi')
+        ->select('tb_comuna.*',"tb_municipio.muni_nomb")
+        ->get();
+
+        return view('comuna.index',['comunas'=> $comunas]);
     }
 
     /**
@@ -98,6 +113,14 @@ class ComunaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi','=','tb_municipio.muni_codi')
+        ->select('tb_comuna.*',"tb_municipio.muni_nomb")
+        ->get();
+
+        return view('comuna.index',['comunas'=> $comunas]);
     }
 }
